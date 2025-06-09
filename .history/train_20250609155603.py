e75a4c1bcc78ba9e17af8c04dd16328b2d153b51
nugahh <fwong@42.student.fr>
2025-06-09 19:59:27 +0200

    add linear reg algo & graph

--- .history/train_20250609155603.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data.csv')
-mileages = df['mileage'].tolist()
-prices = df[;]
-print(df)
--- .history/train_20250609155613.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data.csv')
-mileages = df['km'].tolist()
-prices = df['price'].tolist()
-print(mileages)
